d2a5dc3c6a7034ea6ef4161f8eabca18b7b56b0b
e6m9 <alostsound@gmail.com>
2023-11-28 20:22:19 -0500

    full functionality complete

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,18 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
-// DONE set up dayJS and UNIX to format the date
-// DONE generate elements to occupy resultsBox
-// DONE generate elements to occupy forecastBox
-// use localStorage to save search history
-// generate elements to display search history
-// style document with 2 elements in one row, the second row being a column
-
 var baseCurrentAPI = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid=2ee7bf2ceaf4b8438eed774d0a68a53a';
 var baseWeatherAPI = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=imperial&appid=2ee7bf2ceaf4b8438eed774d0a68a53a';
 
 var currentData = [];
 var apiData = [];
+var searchHistory = [];
 
 function getLocation() {
   var cityName = document.getElementById('searchFld').value;
 
-  // add functionality to clear all created elements except for search history
+  var textBody = document.getElementById('textBox');
+  var forecastRow = document.getElementById('forecastRow');
+  textBody.innerHTML = '';
+  forecastRow.innerHTML = '';
+
   if (cityName !== '') {
     var geoAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=2ee7bf2ceaf4b8438eed774d0a68a53a';
 
@@ -48,14 +36,6 @@ function getLocation() {
               return response.json();
             })
             .then(function (data) {
-
-              // <div id="resultsBox">
-              //   <div id="textCon"></div>
-              //   <div id="forecastBox">
-              //     <div id="forecastTable"></div>
-              //   </div>
-              // </div>
-
               var name = data.name;
               var unixTimeStamp = data.dt;
               var date = dayjs.unix(unixTimeStamp).format('M/DD/YYYY');
@@ -90,7 +70,11 @@ function getLocation() {
                 humid: humid,
                 wind: wind
               });
+
+              addSearchToHistory(cityName);
+              updateSearchHistoryDisplay();
               console.log('Current Weather:', currentData);
+
             });
 
           fetch(weatherAPI)
@@ -104,13 +88,15 @@ function getLocation() {
                 var date = dayjs.unix(unixTimeStamp).format('M/DD/YYYY');
                 var icon = data.list[i].weather[0].icon;
                 var temp = data.list[i].main.temp;
-                var humid = data.list[i].main.humidity;
                 var wind = data.list[i].wind.speed;
+                var humid = data.list[i].main.humidity;
 
-                var forecastBox = document.getElementById('forecastBox');
+                var forecastRow = document.getElementById('forecastRow');
+                var forecastCol = document.createElement('div');
+                forecastCol.classList.add('col-md-2', 'mb-4');
 
                 var forecastBlock = document.createElement('div');
-                forecastBlock.className = 'forecast-block';
+                forecastBlock.classList.add('card', 'text-center');
 
                 forecastBlock.innerHTML =
                   '<h5>' + name + ' ' + date + '</h5>' +
@@ -118,20 +104,24 @@ function getLocation() {
                   '<p>Temperature: ' + temp + 'F</p>' +
                   '<p>Wind: ' + wind + 'MPH</p>' +
                   '<p>Humidity: ' + humid + '%</p>';
+                  
 
-                forecastBox.appendChild(forecastBlock);
+                forecastCol.appendChild(forecastBlock);
+                forecastRow.appendChild(forecastCol);
 
                 apiData.push({
                   city: name,
                   date: date,
                   icon: icon,
                   temp: temp,
-                  humid: humid,
-                  wind: wind
+                  wind: wind,
+                  humid: humid
                 });
               }
 
+              updateSearchHistoryDisplay();
               console.log('API data:', apiData);
+
             });
 
         } else {
@@ -145,5 +135,40 @@ function getLocation() {
     console.log('City name is missing');
   }
 }
+
+function addSearchToHistory(search) {
+  if (!searchHistory.includes(search)) {
+    searchHistory.unshift(search);
+
+    if (searchHistory.length > 8) {
+      searchHistory.pop();
+    }
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+  }
+}
+
+function updateSearchHistoryDisplay() {
+  var historyDiv = document.getElementById('searchHistory');
+  historyDiv.innerHTML = '';
+  for (var i = 0; i < searchHistory.length; i++) {
+    var searchBtn = document.createElement('button');
+    searchBtn.textContent = searchHistory[i];
+    searchBtn.classList.add('btn', 'btn-secondary', 'mr-2');
+    searchBtn.addEventListener('click', function () {
+      document.getElementById('searchFld').value = this.textContent;
+      getLocation();
+    });
+    historyDiv.appendChild(searchBtn);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var storedSearchHistory = localStorage.getItem('searchHistory');
+  if (storedSearchHistory) {
+    searchHistory = JSON.parse(storedSearchHistory);
+    updateSearchHistoryDisplay();
+  }
+});
+
 
 document.getElementById('searchBtn').addEventListener('click', getLocation);
